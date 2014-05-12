b1d7ccb70648f7d164267d478bedbf59a580737d
Nathanael Burt <nathanael.burt@gmail.com>
2014-05-12 13:26:38 -0600

    added associations to make tests pass

--- app/models/conference_room.rb
+++ app/models/conference_room.rb
@@ -1,2 +1,4 @@
 class ConferenceRoom < ActiveRecord::Base
+  has_many :reservations
+  has_many :meetings, through: :reservations
 end
--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,2 +1,4 @@
 class Meeting < ActiveRecord::Base
+  has_many :reservations
+  has_many :conference_rooms, through: :reservations
 end
